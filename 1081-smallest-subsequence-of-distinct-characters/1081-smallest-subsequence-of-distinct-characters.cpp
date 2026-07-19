791d2a16ca6df267663777af98f0b550331bb8bb
Aman6925 <amanshukla6925@gmail.com>
2026-07-19 18:15:08 +0530

    Time: 0 ms (100%), Space: 8.7 MB (79.99%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int count[26] = {0};
+        bool inStack[26] = {false};
+        string result;
+
+        
+        for (char c : s) {
+            count[c - 'a']++;
+        }
+
+        for (char c : s) {
+            count[c - 'a']--;
+
+            
+            if (inStack[c - 'a']) continue;
+
+           
+            while (!result.empty() && result.back() > c && count[result.back() - 'a'] > 0) {
+                inStack[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+
+            
+            result.push_back(c);
+            inStack[c - 'a'] = true;
+        }
+
+        return result;
+    }
+};
