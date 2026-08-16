022388403787b47254b749d6f895e9d4ff702461
Aman6925 <amanshukla6925@gmail.com>
2026-08-16 14:51:54 +0530

    Time: 4 ms (62.44%), Space: 131.4 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        int zero = cnt[0];
+        int one = cnt[1];
+        int two = cnt[2];
+
+        // Only multiples of 3
+        if (one == 0 && two == 0) {
+            return false;
+        }
+
+        // Even number of 0-remainder stones
+        if (zero % 2 == 0) {
+            return one > 0 && two > 0;
+        }
+
+        // Odd number of 0-remainder stones
+        return abs(one - two) > 2;
+    }
+};
